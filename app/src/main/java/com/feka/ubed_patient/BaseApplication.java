18228caac0e5342575aec771d028fa6cfb1f7af0
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2019-04-13 11:48:38 +0800

    init project

---
+++ app/src/main/java/com/feka/ubed_patient/BaseApplication.java
@@ -0,0 +1,11 @@
+package com.feka.ubed_patient;
+
+import android.app.Application;
+
+public class BaseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
